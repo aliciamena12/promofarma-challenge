7e6fa0cd537e98d3a68c8b4ba3eaa8acb0c6517d
Alicia <aliciamena12@gmail.com>
2024-08-09 00:55:59 +0200

    Added fontawesome and image domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["media.docmorris.de"],
+        domains: ["media.docmorris.de", "s3-alpha-sig.figma.com"],
         formats: ["image/avif", "image/webp"],
     },
 };
